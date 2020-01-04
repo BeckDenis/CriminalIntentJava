446d7cda2532e62b734280fd718e50c1b6359b0d
Denis <denisbeck555@gmail.com>
2020-01-04 18:51:01 +0600

    something

--- app/src/main/java/com/example/android/criminalintentjava/MainActivity.java
+++ app/src/main/java/com/example/android/criminalintentjava/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.android.criminalintentjava;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    private Crime mCrime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
     }
 }
